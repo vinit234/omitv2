771d46b23dee9f6a039b976cdae03e448205200f
vinit234 <vinitv275@gmail.com>
2025-04-22 01:15:14 +0530

    first

--- omegle/server.js
+++ omegle/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "https://omitv2.onrender.com",
     methods: ["GET", "POST"]
   }
 });

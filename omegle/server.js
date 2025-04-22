c2953d0033ad9f88312774875e7fda4ab083bdc0
vinit234 <vinitv275@gmail.com>
2025-04-22 12:39:36 +0530

    sp

--- omegle/server.js
+++ omegle/server.js
@@ -34,43 +34,43 @@
 //   socket.on('findPartner', (data) => {
 //     const { chatType } = data;
 //     console.log(`User ${socket.id} attempting to join ${chatType} queue`);
-  
+
 //     // If user is already connected or in queue, do nothing
 //     if (activeConnections.has(socket.id)) {
 //       console.log(`User ${socket.id} is already in an active connection.`);
 //       return;
 //     }
-  
+
 //     if (waitingUsers[chatType].has(socket.id)) {
 //       console.log(`User ${socket.id} is already in the ${chatType} queue.`);
 //       return;
 //     }
-  
+
 //     // Add user to the queue
 //     waitingUsers[chatType].add(socket.id);
 //     console.log(`User ${socket.id} added to ${chatType} queue`);
-  
+
 //     // Try to find a partner
 //     const users = Array.from(waitingUsers[chatType]);
 //     const availablePartner = users.find(id => id !== socket.id && !activeConnections.has(id));
-  
+
 //     if (availablePartner) {
 //       // Remove both users from the queue
 //       waitingUsers[chatType].delete(socket.id);
 //       waitingUsers[chatType].delete(availablePartner);
-  
+
 //       // Store the connection
 //       activeConnections.set(socket.id, { partnerId: availablePartner, chatType });
 //       activeConnections.set(availablePartner, { partnerId: socket.id, chatType });
-  
+
 //       console.log(`Matched users: ${socket.id} and ${availablePartner}`);
-  
+
 //       // Notify both users
 //       io.to(socket.id).emit('partnerFound', { partnerId: availablePartner, chatType });
 //       io.to(availablePartner).emit('partnerFound', { partnerId: socket.id, chatType });
 //     }
 //   });
-  
+
 //   // Handle text messages
 //   socket.on('message', (data) => {
 //     const { partnerId, message } = data;
@@ -89,16 +89,16 @@
 //   socket.on('next', (data) => {
 //     const { chatType } = data;
 //     console.log(`User ${socket.id} clicked next`);
-    
+
 //     const connection = activeConnections.get(socket.id);
 //     if (connection) {
 //       const { partnerId } = connection;
 //       console.log(`Notifying partner ${partnerId} about disconnect`);
 //       io.to(partnerId).emit('partnerLeft');
-      
+
 //       // Add user back to queue
 //       waitingUsers[chatType].add(socket.id);
-      
+
 //       // Clean up connections
 //       activeConnections.delete(socket.id);
 //       activeConnections.delete(partnerId);
@@ -108,20 +108,20 @@
 //   // Handle disconnection
 //   socket.on('disconnect', () => {
 //     console.log('User disconnected:', socket.id);
-    
+
 //     // Remove from all queues
 //     Object.values(waitingUsers).forEach(queue => queue.delete(socket.id));
-    
+
 //     // Handle active connection
 //     const connection = activeConnections.get(socket.id);
 //     if (connection) {
 //       const { partnerId, chatType } = connection;
 //       console.log(`Notifying partner ${partnerId} about disconnect`);
 //       io.to(partnerId).emit('partnerLeft');
-      
+
 //       // Add partner back to queue
 //       waitingUsers[chatType].add(partnerId);
-      
+
 //       // Clean up connections
 //       activeConnections.delete(socket.id);
 //       activeConnections.delete(partnerId);
@@ -198,30 +198,41 @@ io.on('connection', (socket) => {
   });
 
   socket.on('signal', ({ partnerId, signal }) => {
-    io.to(partnerId).emit('signal', { signal, senderId: socket.id });
+    if (activeConnections.has(socket.id) &&
+      activeConnections.get(socket.id).partnerId === partnerId) {
+      io.to(partnerId).emit('signal', { signal, senderId: socket.id });
+    }
+    // io.to(partnerId).emit('signal', { signal, senderId: socket.id });
   });
 
   socket.on('next', ({ chatType }) => {
     console.log(`User ${socket.id} clicked next`);
     const connection = activeConnections.get(socket.id);
+
     if (connection) {
       const { partnerId } = connection;
 
-      // Notify partner
+      // Notify both users
       io.to(partnerId).emit('partnerLeft');
+      io.to(socket.id).emit('partnerLeft');
 
-      // Clean up
+      // Clean up current connections
       activeConnections.delete(socket.id);
       activeConnections.delete(partnerId);
 
-      // Add both back to queue
+      // Add the requesting user back to queue
       waitingUsers[chatType].add(socket.id);
-      waitingUsers[chatType].add(partnerId);
 
-      // Attempt to re-match both
-      matchPartner(socket, chatType);
+      // For the other user, only add them if they're still connected
       const partnerSocket = io.sockets.sockets.get(partnerId);
-      if (partnerSocket) matchPartner(partnerSocket, chatType);
+      if (partnerSocket) {
+        waitingUsers[chatType].add(partnerId);
+        // Try to match the partner with someone else
+        matchPartner(partnerSocket, chatType);
+      }
+
+      // Try to match the user who clicked next
+      matchPartner(socket, chatType);
     }
   });
 
